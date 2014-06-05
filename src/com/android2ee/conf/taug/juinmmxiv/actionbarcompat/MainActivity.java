818a46ddd5017edb09b5429370d9b4162ff6231f
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2014-06-05 18:09:34 +0200

    Ajout des slides et de l'actionMode

--- src/com/android2ee/conf/taug/juinmmxiv/actionbarcompat/MainActivity.java
+++ src/com/android2ee/conf/taug/juinmmxiv/actionbarcompat/MainActivity.java
@@ -3,6 +3,8 @@ package com.android2ee.conf.taug.juinmmxiv.actionbarcompat;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.view.ActionMode;
+import android.support.v7.view.ActionMode.Callback;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
 	EditText edtSearch;
 	MenuItem item;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,7 +28,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		 item = menu.findItem(R.id.action_search);
+		item = menu.findItem(R.id.action_search);
 		RelativeLayout lil = (RelativeLayout) MenuItemCompat.getActionView(item);
 		edtSearch = (EditText) lil.findViewById(R.id.edt_search);
 		Button btnSearch = (Button) lil.findViewById(R.id.btn_search);
@@ -53,6 +56,28 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Toast.makeText(this, "OK ", Toast.LENGTH_LONG).show();
+		if (item.getItemId() == R.id.action_settings) {
+			startSupportActionMode(new Callback() {
+				@Override
+				public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+					return false;
+				}
+				@Override
+				public void onDestroyActionMode(ActionMode mode) {
+				}
+				@Override
+				public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+					getMenuInflater().inflate(R.menu.actionmode_menu, menu);
+					return true;
+				}
+				@Override
+				public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+					mode.finish();
+					return true;
+				}
+
+			});
+		}
 		return super.onOptionsItemSelected(item);
 	}
 
